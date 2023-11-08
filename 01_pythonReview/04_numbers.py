4ccd080f7f0f0461025bd8677884989c6ec13c64
nicolevenachi <ludwing@unicauca.edu.co>
2023-11-08 13:18:57 -0500

    Python review -01- Data types -> Numbers and methods for numbers

---
+++ 01_pythonReview/04_numbers.py
@@ -0,0 +1,30 @@
+
+number = 79
+
+# **** Numbers review
+
+# --operadores
+
+print(number)
+
+print(78 + 22)
+print(78 - 22)
+print(78 * 22)
+print(20 / 6)
+print(20 % 6) #residuo
+
+# -- built-in methods para numbers
+print(abs(-5)) #valor absoluto
+print(max(4,2,-1, 16))  #valor maximo
+print(min(4,2,-1, 16))  #valor minimo
+
+print(round(3.5))  #redondeo
+
+print(bin(3))  #casteo a binary string
+print('number is: ' + str(number)) #casting to str
+
+# -- Methods del paquete math
+from math import * #traigo todo de la libreria math
+
+print(sqrt(9))
+

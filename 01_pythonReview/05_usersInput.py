24c88080e7f56b79996caa8e0217a1155e2373c5
nicolevenachi <ludwing@unicauca.edu.co>
2023-11-08 13:24:28 -0500

    Python review -01- Data types -> users input review

---
+++ 01_pythonReview/05_usersInput.py
@@ -0,0 +1,7 @@
+
+#catching users input
+#whateverVar = input('Message to show: ')
+name = input('Input your name: ')
+age = int(input('Input your age: '))
+
+print('Your name is ' + name + ' and you\'re ' + str(age) + 'years old')

8473ffbecbcd5e5826c58864bbaaa81b05d4af8a
nicolevenachi <ludwing@unicauca.edu.co>
2023-11-09 14:07:22 -0500

    Python review -04- Loops -> for statemnt

--- 01_pythonReview/14_while.py
+++ 01_pythonReview/14_while.py
@@ -3,6 +3,7 @@
 
 i=1 # var to loop over
 
-while i <= 10:
+while i <= 10: #(condition)
   print(i)
   i += 1 #increment the var
+
---
+++ 01_pythonReview/15_forLoop.py
@@ -0,0 +1,36 @@
+
+# *** For sintaxis ***
+
+# over a string
+for letter in 'Hello':
+  print(letter)
+
+# over a list
+myList = ['ji', 'ju', 'jo']
+
+for element in myList:
+
+  if element == 'ji':
+    continue # skip
+  elif element == 'ju':
+    pass  # does nothing
+  else:
+    break # finishes the iteration
+
+  
+  print(element) #toma cada value
+
+# over a dictionary
+myDict = {
+  'name': 'jhon',
+  'age': 25	
+}
+
+for element in myDict:
+  print(element) #toma cada key
+
+# over a list of numbers
+for element in range(10):
+  print(element)
+else:
+  print('finished looping')

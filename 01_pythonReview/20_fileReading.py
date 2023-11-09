405a56e43e491dbc5d6b3545ebab38f68a00f2d7
nicolevenachi <ludwing@unicauca.edu.co>
2023-11-09 15:01:23 -0500

    Python review -06- Files -> Reading

---
+++ 01_pythonReview/20_fileReading.py
@@ -0,0 +1,19 @@
+
+# open(filename, 'mode')
+# Modes ----> -(r)eading, -(w)rite, -(a)ppend to the end of the file,  -(r+) reand andd write capabilities
+
+countr_file = open('./countries.txt', 'r') # abrir the file
+
+print(countr_file.readable()) # saber si el file es leible (permisos)
+
+print(countr_file.readline()) # leo linea a lina, una a una (queda almacenado ultima leida, header queda alli para proxima lectura)
+
+print(countr_file.readlines()) # devuelvo todas las líneas en una list (un list element por cada linea)
+
+countr_file.seek(0,0) #reset a first line
+
+for line in countr_file.readlines():
+  print(line)
+
+countr_file.close()# cerrar the file
+   

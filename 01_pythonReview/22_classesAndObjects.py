22d1fbd9eaec76ded5a55edbe180d73766e3409d
nicolevenachi <ludwing@unicauca.edu.co>
2023-11-09 15:24:34 -0500

    Python review -07- POO -> Clases and Objects

---
+++ 01_pythonReview/22_classesAndObjects.py
@@ -0,0 +1,22 @@
+
+# Creo clase
+class Person:
+  def __init__(self, name, age):
+    # constructor (for received arguments)
+    self.name = name
+    self.age = age
+  
+  #default arguments
+
+# Creo objeto
+#name = input('Entr you name: \t')
+#age = input('Entr you age: \t')
+
+person1 = Person('Isabela', '30') # cinstancia de la clase
+
+#accesar atributos/métodos dle objeto
+print(person1.name) 
+
+#puedo eliminar atributos 
+del person1.name
+print(type(person1))

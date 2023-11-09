1426dd272809784abdc53d1dd9a0d474d2b1ea15
nicolevenachi <ludwing@unicauca.edu.co>
2023-11-09 04:59:30 -0500

    Python review -01- Data types -> Tuples methods

---
+++ 01_pythonReview/09_tuples.py
@@ -0,0 +1,17 @@
+
+#creacion normal brackets ---> (elems,..) | or | tuple constructor ---> tuple((elems,...))
+
+three_numbers = (1, 2, 3, 1, 'last') # creacion con ()
+boo = tuple((True, False, True)) # creacion con constructor
+print(three_numbers)
+print(boo)
+
+#indexing
+print(three_numbers[0])
+
+
+#methods to apply to a list
+print(len(three_numbers))
+print(type(three_numbers)) #tipo de la estructura de datos
+print(type(three_numbers[-1]))
+

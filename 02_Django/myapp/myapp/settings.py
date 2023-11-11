8816dbf8b16b629e93dda6fa0e8c481279fc4765
nicolevenachi <ludwing@unicauca.edu.co>
2023-11-11 14:55:30 -0500

    Django Framework -01- Introduction -> Static Files (me dio perea arreglar todos los llamados a los estaticos en el index.)

--- 02_Django/myapp/myapp/settings.py
+++ 02_Django/myapp/myapp/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os # to use Statics
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),) # agrego al path(BASE_DIR(donde esta), como se llama)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

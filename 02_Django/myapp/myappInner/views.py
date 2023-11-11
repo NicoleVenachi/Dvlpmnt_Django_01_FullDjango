888b3acaaabf91e4122a60ac74a0c68e866ab09d
nicolevenachi <ludwing@unicauca.edu.co>
2023-11-11 09:22:52 -0500

    Django Framework -01- Introduction -> Sending dymanic data to template files

--- 02_Django/myapp/myappInner/views.py
+++ 02_Django/myapp/myappInner/views.py
@@ -4,5 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
   # return HttpResponse('<h1>Welcome </h1>')
-  return render(request, 'index.html')
+
+  context = {
+    'name': 'Maria',
+    'age': 25,
+    'nationality': 'British',
+  }
+  
+  return render(request, 'index.html', context)
 

a16cfd3fe6d31d7bacd3ea72e7155ed89b76d46c
nicolevenachi <ludwing@unicauca.edu.co>
2023-11-17 16:00:41 -0500

    Django Framework -04- User registration -> html template setted up, and form receiving routing using POST metod

--- 02_Django/myapp/myappInner/urls.py
+++ 02_Django/myapp/myappInner/urls.py
@@ -4,6 +4,7 @@ from . import views #importo views de este mismo forde
 
 urlpatterns = [
   path('', views.index, name = 'index'), # (path, handler), '' es la root
-  path('counter', views.counter, name = 'counter')
+  path('counter', views.counter, name = 'counter'),
+  path('register', views.register, name = 'register') #add route for registre
 ]
 
--- 02_Django/myapp/myappInner/views.py
+++ 02_Django/myapp/myappInner/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 from .models import Feature# import de los models 
 
@@ -14,6 +17,24 @@ def index(request):
   }
 
   return render(request, 'index.html', context)
+
+
+def register(request):
+
+  #POST methods routing for this view
+  if request.method == 'POST':
+    username = request.POST.get('username')  
+    email = request.POST.get('email')  
+    password = request.POST.get('password')  
+    password2 = request.POST.get('password2')  
+
+    #verifco passwords are equal
+    if password == password2:
+
+
+    #almaceno data en DB (moder user que es el by default)
+
+  return render(request, 'register.html') #si lo acceso normal, simplemlente traigo el html
 
 def counter(request):
 

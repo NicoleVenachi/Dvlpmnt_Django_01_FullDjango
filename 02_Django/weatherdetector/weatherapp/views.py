f8cf2f04aa0bf3d91d287665d87581ac8111a94d
nicolevenachi <ludwing@unicauca.edu.co>
2023-11-21 07:27:31 -0500

    projects -01- Weatherdetector project -> Configuration to use .env variables

--- 02_Django/weatherdetector/weatherapp/views.py
+++ 02_Django/weatherdetector/weatherapp/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
 
+import json 
+import urllib.request # to make fetch request
+
+import os # to read .env variables
 # Create your views here.
 def index(request):
-  return render(request, 'index.html')
+  api_key = os.environ.get('API_KEY')
+  print(api_key)
+  # POST method routing
+  if request.method == 'POST':
+    city = request.POST.get('city') #get the city from the query params
+    # res = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=%s&appid=' % city).read() #(q)uery (the city)
+    # print(city)
+
+    # json_data = json.loads(res) # json -> dict
+
+    # print(json_data)
+
+    # data = {
+    #   "country_code": str(json_data["sys"]["country"])
+    # }
+
+  # GET method routing
+  else:
+    pass
+  return render(request, 'index.html', {'city': city})
--- 02_Django/weatherdetector/weatherdetector/settings.py
+++ 02_Django/weatherdetector/weatherdetector/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+#imports para usar .env variables
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h5q0i%k#xa5uz!0qx8bz#i50a9-zkx@9ruv3qw+(x3il01(ve6'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

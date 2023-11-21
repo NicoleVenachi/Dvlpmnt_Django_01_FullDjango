a801ec97ab2111f6a91ba879b7a7ef8591558178
nicolevenachi <ludwing@unicauca.edu.co>
2023-11-21 14:59:02 -0500

    projects -01- Realtime channels chat project -> /send endpoint created, to store new room messages. In the frontend, the post form is working sending data from the new user messages

--- 02_Django/djangochat/chat/urls.py
+++ 02_Django/djangochat/chat/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('<str:room>/', views.room, name='room'),
+    path('<str:room>/', views.room, name='room'), # rooms templates routing
     path('checkview', views.checkview, name='checkview'), # route to handle post petitions to accessing a room
+    path('send', views.send, name='send'), #route to handle POST request to store data from new messages in the DB
 ]
--- 02_Django/djangochat/chat/views.py
+++ 02_Django/djangochat/chat/views.py
@@ -2,13 +2,24 @@ from django.shortcuts import render, redirect
 
 from chat.models import Room, Message
 
+from django.http import HttpResponse
+
+
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
 
 def room(request, room):
+
+  username = request.GET.get('username')
+
+  room_details = Room.objects.get(name=room)
   
-  return render(request, 'room.html')
+  return render(request, 'room.html', {
+    "username": username,
+    "room": room,
+    "room_details": room_details
+  })
 
 def checkview(request):
   #get the room and username from the request
@@ -22,6 +33,19 @@ def checkview(request):
   else:
     pass
   
+  #at the end, route to the room
   return redirect('/'+ room + '/?username=' + username)
   
   
+def send(request):
+  #get the room, username and message from the request
+  room_id = request.POST.get('room_id') # mismo que el name room
+  username = request.POST.get('username')
+  message = request.POST.get('message')
+
+  #creo objeto y lo almaceno
+  new_message = Message.objects.create(value = message, user = username, room = room_id)
+  new_message.save()
+  
+  #at the end, send a http response message 
+  return HttpResponse('Message sent successfully')

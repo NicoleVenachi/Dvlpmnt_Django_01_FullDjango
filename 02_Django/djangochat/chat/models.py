37c02eff1a7182f913d83dc64b95081cd58194a3
nicolevenachi <ludwing@unicauca.edu.co>
2023-11-21 11:06:26 -0500

    projects -01- Realtime channels chat project ->  Schemas setted up and DB migrated

--- 02_Django/djangochat/chat/models.py
+++ 02_Django/djangochat/chat/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Room(models.Model): # model for the channel - room
+  # id created by default
+  name = models.CharField(max_length=100)
+  
+class Message(models.Model): # model for store the messages
+  value = models.CharField(max_length=1000000) # message value
+  date = models.DateField(default=datetime.now, blank=True)
+  user = models.CharField(max_length=1000) #podria hacer un fkey de user, igual con la room
+  room = models.CharField(max_length=100)

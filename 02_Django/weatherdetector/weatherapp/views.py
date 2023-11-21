0699a1c73f4c311aeeaa88fadce6021871ecfb1a
nicolevenachi <ludwing@unicauca.edu.co>
2023-11-21 07:54:13 -0500

    projects -01- Weatherdetector project -> Fetching API, and returning data to the template

--- 02_Django/weatherdetector/weatherapp/views.py
+++ 02_Django/weatherdetector/weatherapp/views.py
@@ -6,23 +6,31 @@ import urllib.request # to make fetch request
 import os # to read .env variables
 # Create your views here.
 def index(request):
-  api_key = os.environ.get('API_KEY')
-  print(api_key)
+  
   # POST method routing
   if request.method == 'POST':
+    api_key = os.environ.get('API_KEY')
     city = request.POST.get('city') #get the city from the query params
-    # res = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=%s&appid=' % city).read() #(q)uery (the city)
-    # print(city)
 
-    # json_data = json.loads(res) # json -> dict
+    fetchURL = 'http://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s' %(city,api_key)
+    res = urllib.request.urlopen(fetchURL).read() #(q)uery (the city)
+    
+
+    json_data = json.loads(res) # json -> dict
 
     # print(json_data)
 
-    # data = {
-    #   "country_code": str(json_data["sys"]["country"])
-    # }
+    data = {
+      "country_code": str(json_data["sys"]["country"]),
+      "coordinate": str(json_data["coord"]["lon"])+str(json_data["coord"]["lat"]),
+      "temp": str(json_data["main"]["temp"] - 273.15) + ' C', #temp is in kelvin
+      "pressure": str(json_data["main"]["pressure"]),
+      "humidity": str(json_data["main"]["humidity"])
+    }
 
   # GET method routing
   else:
-    pass
-  return render(request, 'index.html', {'city': city})
+    city = ''
+    data = {}
+
+  return render(request, 'index.html', {'city': city, 'data': data})

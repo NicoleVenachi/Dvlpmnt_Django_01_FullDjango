7c4ad6d81063d34207bc2234dc460b7cf927644c
nicolevenachi <ludwing@unicauca.edu.co>
2023-11-21 06:08:41 -0500

    projects -01- Blog project -> /, querying all the posts and showcasing them on the index.html

--- 02_Django/blog/posts/views.py
+++ 02_Django/blog/posts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def index(request):
-  return render(request, 'index.html')
+  posts = Post.objects.all() # queryng all the posts
+  print(posts)
+  return render(request, 'index.html', {'posts': posts})

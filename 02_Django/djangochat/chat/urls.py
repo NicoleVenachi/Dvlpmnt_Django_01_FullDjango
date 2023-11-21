433d3b025c2f62c2f7dbc3bfaa6bd410c47f8f2c
nicolevenachi <ludwing@unicauca.edu.co>
2023-11-21 11:23:22 -0500

    projects -01- Realtime channels chat project -> /checkview endpoint creted, to join user to an existing to room or to a new one they do create

--- 02_Django/djangochat/chat/urls.py
+++ 02_Django/djangochat/chat/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('<str:room>/', views.room, name='room'),
+    path('checkview', views.checkview, name='checkview'), # route to handle post petitions to accessing a room
 ]
--- 02_Django/djangochat/chat/views.py
+++ 02_Django/djangochat/chat/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from chat.models import Room, Message
 
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
+
+def room(request, room):
+  
+  return render(request, 'room.html')
+
+def checkview(request):
+  #get the room and username from the request
+  room = request.POST.get('room_name')
+  username = request.POST.get('username')
+
+  #check if the rooms exists (if not create it)
+  if not Room.objects.filter(name=room).exists():
+    new_room = Room.objects.create(name=room) #creo la room
+    new_room.save()
+  else:
+    pass
+  
+  return redirect('/'+ room + '/?username=' + username)
+  
+  

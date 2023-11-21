dc091aa2f1c58e35a2aad1371190157299f24540
nicolevenachi <ludwing@unicauca.edu.co>
2023-11-21 06:42:08 -0500

    projects -01- Blog project -> /Post/:id dynamic route creted and handled showing particular .htmls

--- 02_Django/blog/posts/views.py
+++ 02_Django/blog/posts/views.py
@@ -4,5 +4,9 @@ from .models import Post
 # Create your views here.
 def index(request):
   posts = Post.objects.all() # queryng all the posts
-  print(posts)
+  #print(posts)
   return render(request, 'index.html', {'posts': posts})
+
+def post(request, pk):
+  post = Post.objects.get(id=pk) #queryng ONE post, by id
+  return render(request, 'posts.html', {'posts': post})

0f23eb3b5a6ecff11da7fec4492db8f7d40e9a09
nicolevenachi <ludwing@unicauca.edu.co>
2023-11-21 11:08:27 -0500

    projects -01- Realtime channels chat project ->  Superuser created and showcasing the Room and Message models in the admin panel

--- 02_Django/djangochat/chat/admin.py
+++ 02_Django/djangochat/chat/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Room, Message
 
 # Register your models here.
+admin.site.register(Room) # adding the Room model to showcasing them in the admin panel
+admin.site.register(Message)

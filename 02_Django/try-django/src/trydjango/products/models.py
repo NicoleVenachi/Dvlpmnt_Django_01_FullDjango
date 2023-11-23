be6b13688ad96279699a8a6a807701dff45772d8
nicolevenachi <ludwing@unicauca.edu.co>
2023-11-23 15:09:46 -0500

    Django Framework review -03- updating models

--- 02_Django/try-django/src/trydjango/products/models.py
+++ 02_Django/try-django/src/trydjango/products/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
   title = models.CharField(max_length=120)
-  description = models.TextField(blank=True, null=True) #it can be b;ank, empty or null ('' or null)
+  description = models.TextField(blank=True, null=True) #it can be b;ank, empty or null ('' este valor pero es reuiered or null)
   price = models.DecimalField(max_digits=10000, decimal_places=2) #
-  summary = models.TextField()
+  summary = models.TextField(blank=False, null=False)
 
+  featured = models.BooleanField(default=False) # null or default

e4246dfe5d6684203572f8cd120dc06876c6cf7d
nicolevenachi <ludwing@unicauca.edu.co>
2023-11-23 15:35:58 -0500

    Django Framework review -04- Consideraciones -> temapltes, vistas en MVC

--- 02_Django/try-django/src/trydjango/trydjango/settings.py
+++ 02_Django/try-django/src/trydjango/trydjango/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'trydjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'], #base_dir eis the path to tempaltes
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

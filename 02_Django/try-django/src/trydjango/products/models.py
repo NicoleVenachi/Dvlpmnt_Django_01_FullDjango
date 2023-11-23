4260fba58fc7517d23a3a173dbfb5662e2f4f864
nicolevenachi <ludwing@unicauca.edu.co>
2023-11-23 13:58:45 -0500

    Django Framework review -02-Rewritting models

--- 02_Django/try-django/src/trydjango/products/models.py
+++ 02_Django/try-django/src/trydjango/products/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-  title = models.TextField()
-  description = models.TextField()
-  price = models.TextField()
-  summary = models.TextField(default='whatever')
+  title = models.CharField(max_length=120)
+  description = models.TextField(blank=True, null=True) #it can be b;ank, empty or null ('' or null)
+  price = models.DecimalField(max_digits=10000, decimal_places=2) #
+  summary = models.TextField()
 

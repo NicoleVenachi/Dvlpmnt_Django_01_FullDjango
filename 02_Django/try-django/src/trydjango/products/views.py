6a6b19fe0112cf56550159ecf86b583ec852616a
nicolevenachi <ludwing@unicauca.edu.co>
2023-11-24 15:53:45 -0500

    Django Framework review -05- Templates -> Rendering data from DB models

--- 02_Django/try-django/src/trydjango/products/views.py
+++ 02_Django/try-django/src/trydjango/products/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+from .models import Product
+
+
 # Create your views here.
+def product_detail_view(request): #not upper case functinos, and explicit to what it is
+
+  product_obj = Product.objects.get(id=1)
+
+  # context = {
+  #   "title": product_obj.title,
+  #   "description": product_obj.description,
+  # }
+
+  # mas adecuado por si cambio mi dodel o quiero agregar mas models
+  context = {
+    "obj" : product_obj,
+  }
+  return render(request, "product/detail.html", context)
--- 02_Django/try-django/src/trydjango/trydjango/urls.py
+++ 02_Django/try-django/src/trydjango/trydjango/urls.py
@@ -15,14 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path 
 
-from pages import views
+from pages.views import home_view, contact_view, about_view, social_view
+
+from products.views import product_detail_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home_view, name='home'),
-    path('contact', views.contact_view, name='contact'),
-    path('about', views.about_view, name='about'),
-    path('social', views.social_view, name='social'),
+    path('', home_view, name='home'),
+    path('contact', contact_view, name='contact'),
+    path('about', about_view, name='about'),
+    path('social', social_view, name='social'),
+
+    path('product', product_detail_view, name='product-detail' )
 ]

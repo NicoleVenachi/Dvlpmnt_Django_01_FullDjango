2bef228b9463535610b0bbd0ed6bc8e5ac140207
nicolevenachi <ludwing@unicauca.edu.co>
2023-11-26 11:57:09 -0500

    Django Framework review -08- Function based views and Raw class based views -> Differences among fvb y raw class based views. Also RCBV para detail (read one)

--- 02_Django/try-django/src/trydjango/courses/views.py
+++ 02_Django/try-django/src/trydjango/courses/views.py
@@ -6,3 +6,21 @@ from .models import Course
 # BASE VIEW CLass = VIEW
 
 # Create your views here.
+
+class CourseView(View): #or course detail view
+    template_name = "courses/course_detail.html" # DetailView
+
+    #id =None, argumen is not longer required, none by default
+    def get(self, request, id=None, *args, **kwargs):
+        # GET method
+        if id is not None:
+          obj = get_object_or_404(Course, id=id)
+        context = {'object': obj}
+        # context = {'object': self.get_object()}
+        return render(request, self.template_name, context)
+
+
+# HTTP METHODS
+def my_fbv(request, *args, **kwargs):
+    print(request.method)
+    return render(request, 'about.html', {})
--- 02_Django/try-django/src/trydjango/trydjango/urls.py
+++ 02_Django/try-django/src/trydjango/trydjango/urls.py
@@ -28,5 +28,6 @@ urlpatterns = [
     path('social', social_view, name='social'),
 
     path('products/', include('products.urls')),
-    path('blog/', include('blog.urls'))
+    path('blog/', include('blog.urls')),
+    path('courses/', include('courses.urls'))
 ]

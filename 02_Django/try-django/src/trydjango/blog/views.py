8f9dd9bd1d04d842691f515a7e45843cf2be72e5
nicolevenachi <ludwing@unicauca.edu.co>
2023-11-26 08:24:59 -0500

    Django Framework review -08- Class based views-> List view

--- 02_Django/try-django/src/trydjango/blog/views.py
+++ 02_Django/try-django/src/trydjango/blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+from .models import Article
+
+from django.views.generic import (
+  CreateView, 
+  UpdateView,
+  ListView,
+  DetailView,
+  DeleteView,
+)
 # Create your views here.
+
+class ArticleListView(ListView): #list several
+  template_name = 'articles/article_list.html' # ovewrite the template name
+
+  queryset = Article.objects.all() #necesito pasar un queryset con la data para listar
+
--- 02_Django/try-django/src/trydjango/trydjango/urls.py
+++ 02_Django/try-django/src/trydjango/trydjango/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path('about', about_view, name='about'),
     path('social', social_view, name='social'),
 
-    path('products/', include('products.urls'))
+    path('products/', include('products.urls')),
+    path('blog/', include('blog.urls'))
 ]

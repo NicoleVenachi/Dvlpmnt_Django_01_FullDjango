5c3d20c0eef3522623f46b34918581bc0ecac04c
nicolevenachi <ludwing@unicauca.edu.co>
2023-11-26 07:28:40 -0500

    Django Framework review -07- Routing Handling -> URLs reverse

--- 02_Django/try-django/src/trydjango/products/models.py
+++ 02_Django/try-django/src/trydjango/products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Product(models.Model):
@@ -10,5 +11,5 @@ class Product(models.Model):
   featured = models.BooleanField(default=False) # null or default
 
   def get_absolute_url(self):
-    return f"/products/{self.id}/"
+    return reverse("product-detail", kwargs={"id": self.id})  # f"/products/{self.id}/"
   
